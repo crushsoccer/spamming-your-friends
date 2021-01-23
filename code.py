8611d2d93c7bb52f19cae916a9d626c3640f76ca
crushsoccer <65049660+crushsoccer@users.noreply.github.com>
2021-01-23 12:36:44 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,17 @@
+# Import Modules
 from pynput.keyboard import Key, Controller
 import time
 
-message = "面对疾风吧"
+# Inside the "message" variable, you can put what you are going to text to the user
+message = "Hi"
+
 keyboard = Controller()
 time.sleep(5)
-for num in range(1001):
+for num in range(1001): # In the for loop, you can decide how many times you would like to spam the "message" to the other user by changing the number
     for letter in message:
         keyboard.press(letter)
         keyboard.release(letter)
     keyboard.press(Key.enter)
-    keyboard.release(Key.面对疾风enter)
-    time.sleep(1)
+    keyboard.release(Key.enter) 
+    
+    time.sleep(1) # Wait for 1 second before texting the next text
